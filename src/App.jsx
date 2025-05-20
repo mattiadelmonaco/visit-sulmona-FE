99990ed2199a41e2d2105deb4794de27165b0a27
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-20 11:44:28 +0200

    feat: add react router and react router dom - create default layout, homepage, header and footer components - add in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
+
 export default function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
