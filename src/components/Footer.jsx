646b422b2c36cc6e20d74a51fd1f2a0a60d08a0b
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-20 12:55:43 +0200

    feat: install axios and edit header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="bg-[#99031e] shadow-2xl">
+      <div className="container px-10 mx-auto">
+        <h1>Footer</h1>
+      </div>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "../api/axios";
+
 export default function Header() {
+  const [types, setTypes] = useState([]);
+
+  const fetchTypes = () => {
+    axios.get(`${import.meta.env.VITE_BE_URL}/types`, {}).then((res) => {
+      setTypes(res.data.data);
+    });
+  };
+
+  useEffect(fetchTypes, []);
+
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="bg-[#99031e] shadow-2xl py-5">
+      <div className="container px-10 mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="bg-white rounded-full py-5">
+            <a href="/">
+              <img
+                src="./images/visits-sulmona-logo.svg"
+                alt="Logo Visits Sulmona"
+                width="120px"
+              />
+            </a>
+          </div>
+          <form>
+            <select name="types" id="types">
+              <option value="">---</option>
+              {types.map((type) => {
+                return (
+                  <option key={type.id} value={type.id}>
+                    {type.name}
+                  </option>
+                );
+              })}
+            </select>
+            <button type="submit">Filtra</button>
+          </form>
+          <h1>Header</h1>
+        </div>
+      </div>
     </header>
   );
 }
