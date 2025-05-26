6ba08c594b6dde736266ff4e4cddce7786dabfbd
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-26 17:55:20 +0200

    feat: add function scroll to top to scroll every time i change page

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import BtnScrollToUp from "../components/BtnScrollToUp";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function DefaultLayout() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <main className="min-h-screen">
         <Outlet />
