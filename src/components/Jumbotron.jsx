d103bcf9d2be6e027ed7cc82fe542035b81af22d
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-26 17:51:18 +0200

    feat: add play and pause function to jumbo

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,7 +1,24 @@
+import { useState, useRef } from "react";
+
 export default function Jumbotron() {
+  const [isPlaying, setIsPlaying] = useState(true);
+  const videoRef = useRef(null);
+
+  const togglePlay = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
   return (
     <div className="jumbotron-container">
       <video
+        ref={videoRef}
         className="jumbotron-video"
         src="images/videos/intro.mp4"
         autoPlay
@@ -9,9 +26,19 @@ export default function Jumbotron() {
         loop
         playsInline
       ></video>
-      <div className="jumbotron-overlay">
+      <div className="jumbotron-overlay cursor-pointer" onClick={togglePlay}>
         <h1 className="text-5xl font-bold my-4">Benvenuto su Visits Sulmona</h1>
         <p className="text-xl sm:text-3xl">Scopri le meraviglie della città</p>
+        <button
+          onClick={togglePlay}
+          className="absolute bottom-14 right-4 bg-white bg-opacity-50 p-2 rounded-full hover:bg-opacity-75 transition-all cursor-pointer"
+        >
+          {isPlaying ? (
+            <i class="fa-solid fa-pause text-[#99031e] px-2"></i>
+          ) : (
+            <i class="fa-solid fa-play text-[#99031e] px-2"></i>
+          )}
+        </button>
         <p className="absolute bottom-4 right-4 text-sm text-white">
           Riprese di{" "}
           <a href="https://www.ondatv.tv/" className="underline font-bold">
